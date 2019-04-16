857e1f0c99b6cffb21260fa659dc5b268a86ae5e
terrabyte25 <you@example.com>
2019-04-16 16:27:19 -0500

    Change how file is read

--- app/src/main/java/plugins/PluginReader.java
+++ app/src/main/java/plugins/PluginReader.java
@@ -1,69 +1,67 @@
 package plugins;
 
-import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.BufferedInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
+
 
 public class PluginReader {
 	
 	public static String read(String path) throws IOException {
 
-		int lineNum = 0;
-
 		FileInputStream file = new FileInputStream(path);
-		BufferedReader reader = new BufferedReader(new InputStreamReader(file));
-		String line = reader.readLine();
+
+		BufferedInputStream bis = new BufferedInputStream(file);
+
+		byte[] buffer = new byte[8192];
+
+		bis.read(buffer, 0, buffer.length);
+
+		String line = new String(buffer);
+
 		StringBuilder builder = new StringBuilder();
 
 		if (!line.contains("TES3")) // make sure this actually is a real TES3 data file
 			return "";
 
-		while (line != null || lineNum < 5) {
-			int count = line.split("MAST").length;
-			if (count > 0)
-				for (int i = 0; i < count; i++) {
-					try {
-						String[] data = line.split("MAST");
-						if (data[i].contains(".esp")
-								|| data[i].contains(".ESP")) {
+		int count = line.split("MAST").length;
+		if (count > 0)
+			for (int i = 0; i < count; i++) {
+				try {
+					String[] data = line.split("MAST");
+					if (data[i].contains(".esp")
+							|| data[i].contains(".ESP")) {
 
-							builder.append(data[i].split(".esp")[0].replaceAll(
-									"\\p{Cntrl}", "") + ".esp");
-							builder.append("\n");
-						} else if (data[i].contains(".esm")
-								|| data[i].contains(".ESM")) {
-							builder.append(data[i].split(".esm")[0].replaceAll(
-									"\\p{Cntrl}", "") + ".esm");
-							builder.append("\n");
-						}
-
-						else if (data[i].contains(".omwaddon")) {
-							builder.append(data[i].split(".omwaddon")[0]
-									.replaceAll("\\p{Cntrl}", "") + ".omwaddon");
-							builder.append("\n");
-						} else if (data[i].contains(".omwgame")) {
-							builder.append(data[i].split(".omwgame")[0]
-									.replaceAll("\\p{Cntrl}", "") + ".omwgame");
-							builder.append("\n");
-						}
-
-					} catch (Exception e) {
-						reader.close();
-						return builder.toString();
+						builder.append(data[i].split(".esp")[0].replaceAll(
+								"\\p{Cntrl}", "") + ".esp");
+						builder.append("\n");
+					} else if (data[i].contains(".esm")
+							|| data[i].contains(".ESM")) {
+						builder.append(data[i].split(".esm")[0].replaceAll(
+								"\\p{Cntrl}", "") + ".esm");
+						builder.append("\n");
 					}
+
+					else if (data[i].contains(".omwaddon")) {
+						builder.append(data[i].split(".omwaddon")[0]
+								.replaceAll("\\p{Cntrl}", "") + ".omwaddon");
+						builder.append("\n");
+					} else if (data[i].contains(".omwgame")) {
+						builder.append(data[i].split(".omwgame")[0]
+								.replaceAll("\\p{Cntrl}", "") + ".omwgame");
+						builder.append("\n");
+					}
+
+				} catch (Exception e) {
 				}
-			if (line.contains("NAME"))
-				break;
-			line = reader.readLine();
-			lineNum++;
-		}
+			}
 
 		if (path.contains("Bloodmoon.esm")) {
 			builder.append("Tribunal.esm \n");
 		}
 		
-		reader.close();
+		bis.close();
+		file.close();
 		return builder.toString();
 	}
 }
