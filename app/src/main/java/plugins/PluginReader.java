379466b362fa871967b905988387cd67dc8a3448
terrabyte25 <you@example.com>
2019-04-19 13:06:08 -0500

    RandomAccessFile instead

--- app/src/main/java/plugins/PluginReader.java
+++ app/src/main/java/plugins/PluginReader.java
@@ -1,46 +1,55 @@
 package plugins;
 
-import java.io.FileInputStream;
-import java.io.BufferedInputStream;
+import java.nio.MappedByteBuffer;
+import java.io.RandomAccessFile;
 import java.io.IOException;
-
-import java.util.Arrays;
-import java.util.List;
-
+import java.nio.channels.FileChannel;
 
 public class PluginReader {
-	
+
 	public static String read(String path) throws IOException {
+		RandomAccessFile random = new RandomAccessFile(path, "r");
+		byte[] buff = new byte[4096];
+		random.read(buff);
 
-		FileInputStream file = new FileInputStream(path);
+		String line = new String(buff);
+		StringBuilder builder = new StringBuilder();
 
-		BufferedInputStream bis = new BufferedInputStream(file);
+		int count = line.split("MAST").length;
+		if (count > 0)
+			for (int i = 0; i < count; i++) {
+				try {
+					String[] data = line.split("MAST");
+					if (data[i].contains(".esp")
+							|| data[i].contains(".ESP")) {
 
-		byte[] buffer = new byte[8192];
+						builder.append(data[i].split(".esp")[0].replaceAll(
+								"\\p{Cntrl}", "") + ".esp");
+						builder.append("\n");
+					} else if (data[i].contains(".esm")
+							|| data[i].contains(".ESM")) {
+						builder.append(data[i].split(".esm")[0].replaceAll(
+								"\\p{Cntrl}", "") + ".esm");
+						builder.append("\n");
+					}
 
-		bis.read(buffer, 0, buffer.length);
+					else if (data[i].contains(".omwaddon")) {
+						builder.append(data[i].split(".omwaddon")[0]
+								.replaceAll("\\p{Cntrl}", "") + ".omwaddon");
+						builder.append("\n");
+					} else if (data[i].contains(".omwgame")) {
+						builder.append(data[i].split(".omwgame")[0]
+								.replaceAll("\\p{Cntrl}", "") + ".omwgame");
+						builder.append("\n");
+					}
 
-		String line = new String(buffer);
+				} catch (Exception e) {
 
-		bis.close();
-		file.close();
-
-		if (!line.contains("TES3")) // make sure this actually is a real TES3 data file
-			return "";
-
-		List<String> strings = Arrays.asList( line.replaceAll("^.*?MAST", "")
-			.split("DATA.*?(MAST|$)"));
-
-		String dependencies = "";
-
-		for (String s : strings) {
-			dependencies += (s + "\n");
-		}
-
-		if (path.contains("Bloodmoon.esm")) {
-			dependencies += ("Tribunal.esm \n");
-		}
-
-		return dependencies;
+				}
+			}
+		
+		random.close();
+		
+		return builder.toString();
 	}
 }
